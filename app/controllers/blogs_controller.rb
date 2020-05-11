bc059ffe498889e760b96f9744b976d7c8c13e12
齊藤廉 <saitouren@saitourennoMacBook-Pro.local>
2020-05-11 22:59:03 +0900

    [Finish]4-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,28 @@
 class BlogsController < ApplicationController
-  def index
+  def show
+    @blog = Blog.find(params[:id])
   end
 
-  def show
+  def index
+  	# 記事を全件取得
+  	@blogs = Blog.all
   end
 
   def new
+  	@blog = Blog.new
+  end
+
+  def create
+  	blog = Blog.new(blog_params)
+  	blog.save
+  	redirect_to blog_path(blog.id)
   end
 
   def edit
+  end
+
+  private
+  def blog_params
+  	params.require(:blog).permit(:title, :category, :body)
   end
 end
